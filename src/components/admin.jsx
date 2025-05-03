7af20833415e83b7f49b6a0464ac12352716353f
David Gregorc <davidgregorc@Davids-MacBook-Air.local>
2025-05-03 03:20:18 +0200

    lepši dizajn za admin stran

--- src/components/admin.jsx
+++ src/components/admin.jsx
@@ -3,10 +3,6 @@ import { Link } from "react-router-dom";
 import "./Admin.css";
 import { IoIosAddCircleOutline } from "react-icons/io";
 import { CiViewList } from "react-icons/ci";
-import LogoKlasika from "./smklasik.png";
-import LogoGlasbaM from "./smglasbamladih.png";
-import LogoKreativa from "./smkreativ.png";
-import LogoGostuje from "./smagost.png";
 
 
 
@@ -18,32 +14,32 @@ function Admin() {
                     Admin nadzorna plošča
                 </h1>
 
-                <div className="razlozeni-cikli-wrapper">
+                <div className="admin-plosca">
                     <Link to="/admin/dodaj-koncerte">
-                        <div className="razlozeni-cikli-kartica">
-                            <h1 className="ikone-admin-dodajanje"><IoIosAddCircleOutline /></h1>
-                            <h1>Dodaj/uredi koncert</h1>
+                        <div className="admin-plosca-gumb center">
+                            <h1 className="admin-plosca-ikona"><IoIosAddCircleOutline/></h1>
+                            <h1 className="center">Dodaj/uredi koncert</h1>
                         </div>
                     </Link>
 
                     <Link to="/admin/dodaj-novice">
-                        <div className="razlozeni-cikli-kartica">
-                            <h1 className="ikone-admin-dodajanje"><IoIosAddCircleOutline /></h1>
-                            <h1>Dodaj novico</h1>
+                        <div className="admin-plosca-gumb center">
+                            <h1 className="admin-plosca-ikona"><IoIosAddCircleOutline/></h1>
+                            <h1 className="center">Dodaj novico</h1>
                         </div>
                     </Link>
 
                     <Link to="/admin/pregled-rezervacij">
-                        <div className="razlozeni-cikli-kartica">
-                            <h1 className="ikone-admin-dodajanje"><CiViewList /></h1>
-                            <h1>Pregled rezervacij</h1>
+                        <div className="admin-plosca-gumb center">
+                            <h1 className="admin-plosca-ikona"><CiViewList/></h1>
+                            <h1 className="center">Pregled rezervacij</h1>
                         </div>
                     </Link>
 
                     <Link to="/admin/pregled-abonentov">
-                        <div className="razlozeni-cikli-kartica">
-                            <h1 className="ikone-admin-dodajanje"><CiViewList /></h1>
-                            <h1>Abonenti</h1>
+                        <div className="admin-plosca-gumb center">
+                            <h1 className="admin-plosca-ikona"><CiViewList/></h1>
+                            <h1 className="center">Abonenti</h1>
                         </div>
                     </Link>
                 </div>
