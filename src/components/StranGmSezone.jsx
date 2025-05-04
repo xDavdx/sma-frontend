0a091e5033baac524a2f9dc616848bc89109764c
David Gregorc <davidgregorc@Davids-MacBook-Air.local>
2025-05-04 19:07:35 +0200

    stran sezone pri gm

--- src/components/StranGmSezone.jsx
+++ src/components/StranGmSezone.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { gmSezone } from "./gmSezone"; // Uvozi podatke o sezonah
+import { gmSezone } from "./gmSezone";
+import logo from "./sma-logo-font.png"; // Uvozi podatke o sezonah
 
 const SezonaDetail = () => {
     const { leto } = useParams(); // Pridobi leto iz URL-ja
@@ -12,22 +13,96 @@ const SezonaDetail = () => {
 
     return (
         <section className="sezona-podrobnosti">
-            <div className="sezona-podrobnosti-header">
-                <img src={sezona.logo} alt={sezona.ime} className="sezona-logo" />
-                <h1>{sezona.ime}</h1>
-            </div>
+            <section className="onas-uvod">
+                <div className="onas-uvod-container">
+                    <div className="onas-uvod-left"></div>
 
-            <div className="sezona-podrobnosti-vsebina">
-                <h2>Vsebina</h2>
-                {sezona.vsebina.split('\n').map((line, index) => (
-                    <p style={{ marginBottom: "0.5em" }} key={index}>{line}</p>
-                ))}
-            </div>
+                    <div className="onas-uvod-center">
+                        <h1>{sezona.ime}</h1>
+                    </div>
 
-            <div className="sezona-podrobnosti-slika">
-                <img src={sezona.slika1} alt="Slika 1" className="sezona-slika" />
-                <img src={sezona.slika2} alt="Slika 2" className="sezona-slika" />
-            </div>
+                    <div className="onas-uvod-right">
+                    </div>
+                </div>
+            </section>
+
+            <section className="stran-gm-sezone-vsebina">
+                <div className="sezona-podrobnosti-vsebina">
+                    <h2>Vsebina</h2>
+                    {sezona.vsebina.split('\n').map((line, index) => (
+                        <p style={{ marginBottom: "0.5em" }} key={index}>{line}</p>
+                    ))}
+                </div>
+
+                <div className="sezona-podrobnosti-slika">
+                    <img src={sezona.slika1} alt="Slika 1" className="sezona-slika" />
+                    <img src={sezona.slika2} alt="Slika 2" className="sezona-slika" />
+                </div>
+            </section>
+
+
+
+            <section className="koncert-program center">
+                <div className="izvajalci-pri-programu">
+                    <div className="izvajalci-pri-programu-tekst">
+                        <h1>Izvajalci:</h1>
+                        <hr />
+                        {sezona.izvajalci && sezona.izvajalci.length > 0 && (
+                            <div className="izvajalci-karta">
+                                <ul>
+                                    {sezona.izvajalci.map((oseba, index) => (
+                                        <li key={index}>
+                                            {oseba.ime} – {oseba.instrument}
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        )}
+                    </div>
+                </div>
+
+                <div className="center">
+                    <div className="stran-koncerta-program-wrapper">
+                        <div className="center-program">
+                            <h1>Program:</h1>
+                            <hr style={{ width: "10%" }} />
+                        </div>
+
+                        <div className="array-program">
+                            {sezona.program1 && sezona.program1.length > 0 && (
+                                <div className="sezona-program">
+                                    <ul>
+                                        {sezona.program1.map((item, index) => (
+                                            <li key={index}>
+                                                <strong>{item.skladatelj}</strong> – {item.naslov}
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                </div>
+
+            </section>
+
+
+
+
+            {sezona.linki && sezona.linki.length > 0 && (
+                <div className="sezona-linki">
+                    <h2>Povezave</h2>
+                    <ul>
+                        {sezona.linki.map((item, index) => (
+                            <li key={index}>
+                                <a href={item.link} target="_blank" rel="noopener noreferrer">
+                                    {item.text}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
         </section>
     );
 };
--- src/components/gmSezone.js
+++ src/components/gmSezone.js
@@ -1,25 +1,125 @@
 import logo1 from "./smglasbamladih.png"
+import gm2021 from "./gmSezona2021.png"
+import gm2022 from "./gm2022.png"
 
 
 export const gmSezone = [
     {
         leto: 2021,
         ime: "Sezona 2021",
-        slika1: "path/to/slika1-2021.jpg",
+        slika1: gm2021,
         slika2: "path/to/slika2-2021.jpg",
         logo: logo1,
         naslov: "Glasba mladih",
         vsebina: "Premierna izvedba skupne ideje o skladateljskem koncertu mladih ustvarjalcev. Takratni maturantje Konservatorija za glasbo in balet Ljubljana so na pobudo mladega skladatelja Vida Ožbolta zorganizirali koncert nove komorne glasbe, preostanek skladb so napisali njihovi sošolci in znanci. Na krstnem koncertu v Radovljici je publika napolnila cerkev sv. Petra in mladim dala zagon za nadaljevanje s projektom.\n",
+        program1: [
+            {
+                skladatelj: "Tilen Slakan",
+                naslov: "Godalni kvartet št. 1",
+            },
+            {
+                skladatelj: "Lan Podletnik Ašič",
+                naslov: "Sanje za klavirski trio",
+            },
+            {
+                skladatelj: "Sara Bartha Rijavec",
+                naslov: "Del celote za klavirski kvartet",
+            },
+            {
+                skladatelj: "Lan Podletnik Ašič",
+                naslov: "Del celote za klavirski kvartet",
+            },
+            {
+                skladatelj: "Martin Vasle",
+                naslov: "Tempo de maio za godalni kvartet",
+            },
+            {
+                skladatelj: "Vid Ožbolt",
+                naslov: "Klavirski kvintet ",
+            },
+        ],
+        izvajalci: [
+            { ime: "Neža Tovšak", instrument: "klavir" },
+            { ime: "Timotej Willewaldt", instrument: "violina" },
+            { ime: "Laura de Wolff", instrument: "violina" },
+            { ime: "Manca Kosmač", instrument: "viola" },
+            { ime: "Sofia Grassi", instrument: "violončelo" }
+        ],
+        linki: [
+            {
+                text: "Posnetke koncerta si lahko ogledate na našem YouTube kanalu",
+                link: "https://www.youtube.com/@glasbamladih"
+            },
+            {
+                text: "Klavirski kvintet Vida Ožbolta pa je bil tudi arhivsko posnet in predvajan na Radiu Ars",
+                link: "https://ars.rtvslo.si/podkast/nove-glasbene-generacije/15104115/174846238"
+            }
+        ],
     },
+
+
+
     {
         leto: 2022,
         ime: "Sezona 2022",
-        slika1: "path/to/slika1-2022.jpg",
+        slika1: gm2022,
         slika2: "path/to/slika2-2022.jpg",
         logo: logo1,
         naslov: "Glasba mladih II",
-        vsebina: "Vsebina sezone 2022... \n Dodani novi skladatelji in širša zasedba.",
+        vsebina: "Druga izvedba projekta je prinesla večjo zasedbo, nove skladatelje in več koncertov. Premierni koncert je bil izveden v radovljiški župnijski cerkvi sv. Petra, posnela ga je ekipa RTV Slovenija. Projekt smo predstavili še v dvorani Pokrajinskega muzeja Koper, Dvorani Lucijana Marije Škerjanca na Konservatoriju za glasbo in balet Ljubljana in v Kulturnem domu Postojna.",
+        program1: [
+            {
+                skladatelj: "Krištof Strnad",
+                naslov: "Genesis za klavirski kvintet"
+            },
+            {
+                skladatelj: "Sofia Grassi",
+                naslov: "Za sončnimi okni za flavto, bas klarinet in violončelo"
+            },
+            {
+                skladatelj: "Žiga Čopi",
+                naslov: "Sunflower seeds (Sončnična semena) za godalni kvartet in fagot"
+            },
+            {
+                skladatelj: "Matej Kastelic",
+                naslov: "Agnus Dei (Jagnje Božje) za sopran, klavir in violončelo"
+            },
+            {
+                skladatelj: "Martin Vasle",
+                naslov: "Date (Zmenek) za harfo, flavto, klarinet in godalni kvartet"
+            },
+            {
+                skladatelj: "Vid Ožbolt",
+                naslov: "Tri pesmi za sopran, flavto, klarinet, fagot, harfo, klavir in godalni kvartet"
+            }
+        ],
+        izvajalci: [
+            { ime: "Maja Triler", instrument: "sopran" },
+            { ime: "An Černe", instrument: "flavta" },
+            { ime: "Tadeja Malc", instrument: "klarinet" },
+            { ime: "Eva Fritz", instrument: "fagot" },
+            { ime: "Sara Bajc", instrument: "harfa" },
+            { ime: "Neža Tovšak", instrument: "klavir" },
+            { ime: "Timotej Willewaldt", instrument: "violina" },
+            { ime: "Laura de Wolff", instrument: "violina" },
+            { ime: "Manca Kosmač", instrument: "viola" },
+            { ime: "Sofia Grassi", instrument: "violončelo" },
+            { ime: "Vid Ožbolt", instrument: "dirigent" }
+        ],
+        linki: [
+            {
+                text: "Premierni koncert v Radovljici je posnela ekipa RTV Slovenija, predvajan je bil v dveh oddajah programa ARS, poslušajte si jih na povezavah!\n\n 1. del koncerta",
+                link: "https://ars.rtvslo.si/podkast/nove-glasbene-generacije/15104115/174900231"
+            },
+            {
+                text: "2. del koncerta",
+                link: "https://ars.rtvslo.si/podkast/mladi-virtuozi/15104166/174900969"
+            }
+        ],
     },
+
+
+
     {
         leto: 2023,
         ime: "Sezona 2023",
@@ -29,6 +129,9 @@ export const gmSezone = [
         naslov: "Glasba mladih III",
         vsebina: "Vsebina sezone 2023... \n Uspešni koncerti, snemanja in medijska prepoznavnost.",
     },
+
+
+
     {
         leto: 2024,
         ime: "Sezona 2024",
@@ -38,6 +141,9 @@ export const gmSezone = [
         naslov: "Glasba mladih IV",
         vsebina: "Vsebina sezone 2024... \n Priprava novih projektov in širjenje meja.",
     },
+
+
+
     {
         leto: 2025,
         ime: "Sezona 2025",
